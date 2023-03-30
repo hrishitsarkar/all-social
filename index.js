ecb8a35fa4abdad8aef075c7700e13c4b9f256a0
nothrishit <hrishitjohn@gmail.com>
2023-03-30 13:14:04 +0530

    added home.ejs and rendered

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use('/',require('./routes'));
 app.set('view engine','ejs');
 app.set('views','./views');
 
+
 app.listen(port,function(err){
     if(err) console.log(`Error in running the server : ${err}`);
   console.log(`Server is running on port : ${port}`);
